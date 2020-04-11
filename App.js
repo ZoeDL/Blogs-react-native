97b9a5b365c4dff122c1e8df2863ee2f116cb5e4
zexi yu <eezxyu@gmail.com>
2020-04-11 21:02:16 +0800

    Add context and wrap everthing with context provider.

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import IndexScreen from './src/screens/IndexScreen'
+import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Index">
-        <Stack.Screen
-          name="Index"
-          component={IndexScreen}
-          options={{ title: 'Blogs' }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Index">
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={{ title: 'Blogs' }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   )
 }
 
