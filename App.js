ee3cafce0037fa9d2f7a3ce516fd8bb816191f7f
zexi yu <eezxyu@gmail.com>
2020-04-11 20:32:53 +0800

    Set up the project and configure the react native navigation

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import IndexScreen from './src/screens/IndexScreen'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Index">
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title: 'Blogs' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text>This is index screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IndexScreen;
